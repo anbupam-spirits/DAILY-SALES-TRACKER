978ad0dbe9059c16560ff262ce463b5c48874944
Anupam Borah <anupam@spiritsandmalts.com>
2026-01-22 11:26:19 +0530

    Add system path configuration to app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 import streamlit as st
 st.set_page_config(page_title="Field Sales Reporting", page_icon="🚀", layout="wide")
 
